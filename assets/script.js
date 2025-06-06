0b114984e7d85a10104f84c5cd09dde77fbccaa9
Thesancs <santos.glauber2002@gmail.com>
2025-06-06 12:19:40 -0300

    Integrate ConverteAi player and update CTA

--- assets/script.js
+++ assets/script.js
@@ -7,10 +7,10 @@ async function loadComponent(id, path) {
 document.addEventListener('DOMContentLoaded', async () => {
   await loadComponent('header', './components/header.html');
   await loadComponent('main', './components/video.html');
-  const playerScript = document.createElement('script');
-  playerScript.src = 'https://scripts.converteai.net/c9da0398-934e-4680-99b9-d4888d7094af/players/6842141d891666ff59083fee/player.js';
-  playerScript.async = true;
-  document.head.appendChild(playerScript);
+  const inline = document.getElementById('scr_6842141d891666ff59083fee');
+  if (inline) {
+    eval(inline.innerText);
+  }
   await loadComponent('cta', './components/cta.html');
   await loadComponent('footer', './components/footer.html');
 
